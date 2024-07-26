aabd0cf509fa3c2ebee30bfc07b9c7f5440227a6
Elie Kitadi <elkdev.tutos@gmail.com>
2024-07-26 11:37:51 +0300

    fixed lead series association after creation or edition

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -12,6 +12,35 @@ class Lead extends Model
 
     protected $guarded = [];
 
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(function (Lead $model) {
+            if ($model->series_id) {
+                self::generateSerialNumber($model);
+            }
+        });
+
+        static::saving(function (Lead $model) {
+            if ($model->series_id && !$model->serial_number) {
+                self::generateSerialNumber($model);
+            }
+        });
+    }
+
+
+    /**
+     * @param $model
+     * @return void
+     */
+    private static function generateSerialNumber($model): void
+    {
+        $model->serial_number = Lead::where('series_id', $model->series_id)->max('serial_number') + 1;
+        $model->serial = $model->series->prefix . '-' . str_pad($model->serial_number, 5, '0', STR_PAD_LEFT);
+    }
+
     public function team(): BelongsTo
     {
         return $this->belongsTo(Team::class);
@@ -35,5 +64,10 @@ class Lead extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function series(): BelongsTo
+    {
+        return $this->belongsTo(Series::class);
     }
 }
